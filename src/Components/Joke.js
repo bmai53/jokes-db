a1fed6fe973ed9f67dd44aefb6ef998b33d5abf4
bmai <bmai@sfu.ca>
2021-01-16 05:00:09 -0800

    fix gh pages share link

--- src/Components/Joke.js
+++ src/Components/Joke.js
@@ -30,7 +30,7 @@ export default ({ jokeId }) => {
   };
 
   const shareJoke = () => {
-    copy(window.location.origin + '/#/' + joke._id);
+    copy(window.location.href.split("/#/") + "/#/" + joke._id);
     setShowSnackbar(true);
   };
 
