ffee9a9c479c34aea34fcd6d18b6bcf34207dcec
bmai <bmai@sfu.ca>
2021-01-18 19:03:27 -0800

    added fb login

--- src/Components/AddJoke.js
+++ src/Components/AddJoke.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import axios from "axios";
 import filter from "leo-profanity";
+import AddJokeButtons from "./AddJokeButtons";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import Snackbar from "@material-ui/core/Snackbar";
@@ -12,6 +12,8 @@ import MuiAlert from "@material-ui/lab/Alert";
 export default ({ setAddedJokeId }) => {
   const [joke, setJoke] = useState("");
   const [answer, setAnswer] = useState("");
+  const [userId, setUserId] = useState("");
+
   const [showSuccess, setShowSuccess] = useState(false);
   const [showFail, setShowFail] = useState(false);
 
@@ -24,12 +26,13 @@ export default ({ setAddedJokeId }) => {
         .post(process.env.REACT_APP_BACKEND_API + "add", {
           joke: joke,
           answer: answer,
+          userId: userId,
         })
         .then((result) => {
           console.log("added!", result.data);
           setJoke("");
           setAnswer("");
-          setAddedJokeId(result.data._id)
+          setAddedJokeId(result.data._id);
           setShowSuccess(true);
         })
         .catch((err) => {
@@ -81,19 +84,7 @@ export default ({ setAddedJokeId }) => {
             }}
             variant='outlined'
           />
-          <Button
-            style={{
-              textAlign: "center",
-              margin: "25px 0px 0px 0px",
-            }}
-            variant='contained'
-            color='primary'
-            onClick={(event) => {
-              addJoke(event);
-            }}
-          >
-            Add New Joke
-          </Button>
+          <AddJokeButtons addJoke={addJoke} setUserId ={setUserId}/>
         </CardContent>
       </Card>
       <Snackbar
---
+++ src/Components/AddJokeButtons.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+export default ({ addJoke, setUserId }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const marginStyle = { margin: "50px 0 0" };
+
+  const statusCheckCallBack = (response) => {
+    const { status, authResponse } = response;
+    if (status === "connected") {
+      console.log("fb connected", response);
+      setUserId(authResponse.userID);
+      setLoggedIn(true);
+    }
+  };
+  const fbLogin = () => {
+    FB.login((response) => {
+      statusCheckCallBack(response);
+    });
+  };
+
+  useEffect(() => {
+    if (typeof FB !== "undefined") {
+      FB.getLoginStatus((response) => {
+        statusCheckCallBack(response);
+      });
+    }
+  }, []);
+
+  if (loggedIn) {
+    return (
+      <Button
+        style={{
+          textAlign: "center",
+          width: "300px",
+          height: "50px",
+          ...marginStyle,
+        }}
+        variant='contained'
+        color='primary'
+        onClick={(event) => {
+          addJoke(event);
+        }}
+      >
+        Add New Joke
+      </Button>
+    );
+  } else {
+    return (
+      <>
+        <Button
+          style={{
+            backgroundColor: "#3b5998",
+            textAlign: "center",
+            width: "300px",
+            height: "50px",
+            ...marginStyle,
+          }}
+          variant='contained'
+          color='primary'
+          onClick={() => {
+            fbLogin();
+          }}
+        >
+          Connect with Facebook
+        </Button>
+        <Typography style={{ margin: "24px" }}>
+          Log into Facebook to add new jokes to the database!
+        </Typography>
+      </>
+    );
+  }
+};
